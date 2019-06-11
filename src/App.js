f75c8241abffe00be63b97423aeeb0963c067acb
avinash-dev <avinash@teletext-holidays.co.uk>
2019-06-11 15:31:39 +0530

    Removed old components code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
-import Drawer from './containers/Drawer/Drawer';
-import CustomizedExpansionPanel from './containers/ExpansionPanel/ExpansionPanel';
-import ScrollComponent from './containers/ScrollComponent/ScrollComponent';
-
 class App extends Component{
   render(){
     return (
       <React.Fragment>
-        <ScrollComponent />
+        App
       </React.Fragment>
     );
   }
 }
 
 export default App;
-//        <CustomizedExpansionPanel />
