2a11a1719377b5759d6827ea443d8a804e441b45
avinash-dev <avinash@teletext-holidays.co.uk>
2019-06-21 21:03:58 +0530

    Added react-window-size-listener

--- src/containers/Box/Box.js
+++ src/containers/Box/Box.js
@@ -4,18 +4,22 @@ import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Paper from '@material-ui/core/Paper';
-import WindowSizeListener from 'react-window-size-listener';
+import { withWindowSizeListener } from 'react-window-size-listener';
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1
+    },
+    paper: {
+        height: 140,
+        width: 100,
     },
     control: {
         padding: theme.spacing(2)
     }
 }));
 
-export default function SpacingGrid(){
+function SpacingGrid(props){
     const [spacing, setSpacing] = React.useState(2);
     const classes = useStyles();
 
@@ -24,19 +28,23 @@ export default function SpacingGrid(){
     }
     return(
         <>
-        <WindowSizeListener onResize={windowSize => (
             <Grid container className={classes.root} spacing={2}>
                 <Grid item xs={12}>
-                    <Grid container justify="center" spacing={spacing}>
-                        {[0,1,2].map(value => (
+                    <Grid container 
+                        //  justify="center" 
+                        spacing={spacing}>
+                        {[0,1,2,3,4,5,6,7,8].map(value => (
                             <Grid key={value} item>
-                                <Paper style={{'height': (windowSize.windowHeight)/3, 'width': (windowSize.windowWidth)/3}} />
+                                <Paper style={{'height': '200px', 'width': (props.windowSize.windowWidth)/4, 'border': 'solid 1px #000'}} />
                             </Grid>
                         ))}
                     </Grid>
                 </Grid>
             </Grid>
-        )}/>
         </>
     )
 }
+
+export default withWindowSizeListener(SpacingGrid);
+
+//                                 
